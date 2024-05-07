ffbbe8de262ef8ee44e45ea1253d9383f636e4b6
Prish20 <prish@students.uonbi.ac.ke>
2024-05-07 11:57:00 +0300

    Implement the user api

---
+++ api/v1/views/users.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This module provides a RESTful API for User objects.
+"""
+
+from flask import Flask, jsonify, abort, request, make_response
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a User"""
+    try:
+        data = request.get_json(force=True)
+    except Exception:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'email' not in data:
+        return make_response(jsonify({"error": "Missing email"}), 400)
+    if 'password' not in data:
+        return make_response(jsonify({"error": "Missing password"}), 400)
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    try:
+        data = request.get_json(force=True)
+    except Exception:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
